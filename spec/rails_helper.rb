ca4c37be99c7a4b89f89e8dfab23df0200748383
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-01 13:36:29 -0700

    Add VCR configuration and sensitive data filtering

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -14,6 +14,17 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+# VCR requirements
+require 'vcr'
+require 'webmock/rspec'
+
+# VCR Configuration
+VCR.configure do |config|
+  config.cassette_library_dir = 'spec/cassettes'
+  config.hook_into :webmock
+  config.filter_sensitive_data("<GITHUB_API_KEY>") { ENV['GITHUB_API_KEY'] }
+end
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
