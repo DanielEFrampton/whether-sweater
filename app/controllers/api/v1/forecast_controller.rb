4b0241137cc96823381bcaa0e0994251cce7ce50
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-01 13:21:17 -0700

    Refactor serializer and controller to expect Forecast PORO

--- app/controllers/api/v1/forecast_controller.rb
+++ app/controllers/api/v1/forecast_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::ForecastController < ApplicationController
   def show
-    render json: ForecastSerializer.new
+    forecast = Forecast.new
+    render json: ForecastSerializer.new(forecast)
   end
 end
--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,3 +1,4 @@
 class ForecastSerializer
-
+  include FastJsonapi::ObjectSerializer
+  set_key_transform :camel_lower
 end
