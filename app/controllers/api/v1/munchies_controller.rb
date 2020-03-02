474b9eeaeabffeb54a0940ae82bafce5a06b8316
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-02 10:42:26 -0700

    Add Yelp Service call and pass API data to PORO

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -7,6 +7,7 @@ class Api::V1::MunchiesController < ApplicationController
     # Calculate Unix timestamp of arrival time
     travel_seconds = distance_info['rows'][0]['elements'][0]['duration']['value']
     arrival_timestamp = Time.now.to_i + travel_seconds
+    # Need to account for timezone differences eventually
 
     # Pull latitude and longitude from geocode info
     lat = end_location_info['results'][0]['geometry']['location']['lat']
@@ -17,9 +18,9 @@ class Api::V1::MunchiesController < ApplicationController
     forecast = forecast_info['currently']['summary']
 
     # Get Yelp info on restaurant at end location open at timestamp
-    restaurant_info = YelpService.new.restaurant_open_at(params[:food], arrival_timestamp)
+    restaurant_info = YelpService.new.restaurant_open_at(lat, long, params[:food], arrival_timestamp)
 
-    munchies = Munchies.new(params[:start], params[:end], params[:food])
+    munchies = Munchies.new(forecast, restaurant_info, distance_info)
     render json: MunchiesSerializer.new(munchies)
   end
 end
