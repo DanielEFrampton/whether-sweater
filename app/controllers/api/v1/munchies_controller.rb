317e9e45f7f55d5c815d810fb0b15d94ca90c7d4
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-02 09:43:45 -0700

    Add Google API calls to munchies controller

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,5 +1,9 @@
 class Api::V1::MunchiesController < ApplicationController
   def show
+    # start_location_info = GoogleApiService.new.geocode(params[:start])
+    end_location_info = GoogleApiService.new.geocode(params[:end])
+    distance_info = GoogleApiService.new.distance(params[:start], params[:end])
+    require "pry"; binding.pry
     munchies = Munchies.new(params[:start], params[:end], params[:food])
     render json: MunchiesSerializer.new(munchies)
   end
