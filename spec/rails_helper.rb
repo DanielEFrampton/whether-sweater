36cde9aa56ff84094fd5270dff55049db7f3b389
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-01 19:25:39 -0700

    Add RSpec metadata configuration line

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,6 +22,7 @@ require 'webmock/rspec'
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/cassettes'
   config.hook_into :webmock
+  config.configure_rspec_metadata!
   config.filter_sensitive_data("<GITHUB_API_KEY>") { ENV['GITHUB_API_KEY'] }
 end
 
