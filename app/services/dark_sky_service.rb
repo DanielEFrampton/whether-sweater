2909e1ef8bccfee443ec4ea1eb7d2ff0b4f03193
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-01 18:30:52 -0700

    Create stubbed DarkSky service

--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -1,4 +1,11 @@
 class DarkSkyService
-  def forecast
+  def forecast(lat, long)
+    get_forecast("#{lat},#{long}")
   end
+
+  private
+
+    def get_forecast(lat_long)
+      # build out via TDD via model test
+    end
 end
