d04f84d1a74f6cb641bd06ac4f6993d55ef00a58
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-02 10:59:21 -0700

    Update munchies PORO and serializer with attributes

--- app/poros/munchies.rb
+++ app/poros/munchies.rb
@@ -1,7 +1,11 @@
 class Munchies
-  attr_reader :id
-  
-  def initialize(start_location, end_location, food)
+  attr_reader :id, :forecast, :travel_time, :restaurant
+
+  def initialize(forecast, restaurant_info, travel_time, end_location)
     @id = nil
+    @end_location = end_location
+    @travel_time = travel_time
+    @forecast = forecast
+    @restaurant = restaurant_info
   end
 end
--- app/serializers/munchies_serializer.rb
+++ app/serializers/munchies_serializer.rb
@@ -1,4 +1,4 @@
 class MunchiesSerializer
   include FastJsonapi::ObjectSerializer
-
+  attributes :travel_time, :forecast, :end_location, :restaurant
 end
