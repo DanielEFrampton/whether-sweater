72f4eafd1edf676ed782cfabe2659aaab8012f55
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-01 18:30:43 -0700

    Create Google API Service

--- app/services/google_api_service.rb
+++ app/services/google_api_service.rb
@@ -1,5 +1,12 @@
 class GoogleApiService
-  def geocode
+  def geocode(location)
+    get_geocode(location)
   end
 
+  def get_geocode(location)
+    response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json?address=denver,co') do |request|
+      request.params['key'] = ENV['GOOGLE_API_KEY']
+    end
+    JSON.parse(response.body)
+  end
 end
