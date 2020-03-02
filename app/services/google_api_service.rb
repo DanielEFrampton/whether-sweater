65d16b1ac6a590b56e14f5d5adcbd083eb3ba123
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-02 09:44:14 -0700

    Refactor Google service to have one connection and two requests

--- app/services/google_api_service.rb
+++ app/services/google_api_service.rb
@@ -3,10 +3,26 @@ class GoogleApiService
     get_geocode(location)
   end
 
+  def distance(start_location, end_location)
+    get_distance(start_location, end_location)
+  end
+
+  private
+
   def get_geocode(location)
-    response = Faraday.get('https://maps.googleapis.com/maps/api/geocode/json?address=denver,co') do |request|
-      request.params['key'] = ENV['GOOGLE_API_KEY']
-    end
+    response = connection.get('geocode/json?address=denver,co')
     JSON.parse(response.body)
+  end
+
+  def get_distance(start_location, end_location)
+    response = connection.get("distancematrix/json?units=imperial&origins=#{start_location}&destinations=#{end_location}")
+    JSON.parse(response.body)
+  end
+
+  def connection
+    Faraday.new('https://maps.googleapis.com/maps/api') do |faraday|
+      faraday.adapter Faraday.default_adapter
+      faraday.params['key'] = ENV['GOOGLE_API_KEY']
+    end
   end
 end
