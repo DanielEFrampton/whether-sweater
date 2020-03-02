5a8738aeb33599c24673fe6ebc1f17668d64d530
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-02 10:03:04 -0700

    Refactor DarkSky forecast method to take optional time parameter

--- app/services/dark_sky_service.rb
+++ app/services/dark_sky_service.rb
@@ -1,12 +1,12 @@
 class DarkSkyService
-  def forecast(lat, long)
-    get_forecast("#{lat},#{long}")
+  def forecast(lat, long, time = nil)
+    get_forecast("#{lat},#{long}#{',' + time.to_s if time}")
   end
 
   private
 
-    def get_forecast(lat_long)
-      response = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_SECRET']}/#{lat_long}")
+    def get_forecast(query_params)
+      response = Faraday.get("https://api.darksky.net/forecast/#{ENV['DARK_SKY_SECRET']}/#{query_params}")
       JSON.parse(response.body)
     end
 end
