1e268f96a86e0fabea26cb1104fddf1c28ee4c19
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-02 10:59:39 -0700

    Reorganize munchies controller and refactor Munchies PORO arguments

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,17 +1,16 @@
 class Api::V1::MunchiesController < ApplicationController
   def show
-    # Get necessary Google API info
+    # Get Google Geocode API info
     end_location_info = GoogleApiService.new.geocode(params[:end])
-    distance_info = GoogleApiService.new.distance(params[:start], params[:end])
-
-    # Calculate Unix timestamp of arrival time
-    travel_seconds = distance_info['rows'][0]['elements'][0]['duration']['value']
-    arrival_timestamp = Time.now.to_i + travel_seconds
-    # Need to account for timezone differences eventually
-
-    # Pull latitude and longitude from geocode info
+    end_location = end_location_info['results'][0]['formatted_address']
     lat = end_location_info['results'][0]['geometry']['location']['lat']
     long = end_location_info['results'][0]['geometry']['location']['lng']
+
+    # Get Google Distance Matrix API info (doesn't account for time zone)
+    distance_info = GoogleApiService.new.distance(params[:start], params[:end])
+    travel_time = distance_info['rows'][0]['elements'][0]['duration']['text']
+    travel_seconds = distance_info['rows'][0]['elements'][0]['duration']['value']
+    arrival_timestamp = Time.now.to_i + travel_seconds
 
     # Get DarkSky forecast at given arrival time
     forecast_info = DarkSkyService.new.forecast(lat, long, arrival_timestamp)
@@ -20,7 +19,10 @@ class Api::V1::MunchiesController < ApplicationController
     # Get Yelp info on restaurant at end location open at timestamp
     restaurant_info = YelpService.new.restaurant_open_at(lat, long, params[:food], arrival_timestamp)
 
-    munchies = Munchies.new(forecast, restaurant_info, distance_info)
+    restaurant_name = restaurant_info['businesses'][0]['name']
+    restaurant_address = restaurant_info['businesses'][0]['location']['display_address'].join(', ')
+
+    munchies = Munchies.new(forecast, {name: restaurant_name, address: restaurant_address}, travel_time, end_location)
     render json: MunchiesSerializer.new(munchies)
   end
 end
