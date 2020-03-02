5d550770e3970dba06add556daa51c8a9c5036ba
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-01 18:29:44 -0700

    Remove unnecessary elements from expected results

--- spec/requests/api/v1/forecast_spec.rb
+++ spec/requests/api/v1/forecast_spec.rb
@@ -16,203 +16,110 @@ describe 'As a developer of the front-end' do
       expected_json = JSON.generate({
         'data': {
           'attributes': {
-            'dateTime': {
-              # 'time': '2:08',
-              # 'period': 'PM',
-              # 'date': '2/29',
-              'formattedDateTime': '2:08 PM, 2/29',
-              # 'unixTimestamp': '1583010500',
-              # 'timezone': 'America/Denver',
-              # 'utcHourDifference': -7
-            },
-            'location': {
-              # 'city': 'Denver',
-              # 'state': 'CO',
-              'formattedCityState': 'Denver, CO',
-              'country': 'United States'
-            },
-            'currently': {
-              # 'temperature': 54.3,
-              # 'roundedTemperature': 54,
-              'formattedTemperature': '54°',
-              # 'feelsLike': 54.3,
-              # 'roundedFeelsLike': 54,
-              'formattedFeelsLike': '54°',
-              # 'dayTemperatureHigh': 57.41,
-              # 'dayToundedTemperatureHigh': 57,
-              'formattedDayTemperatureHigh': '57°',
-              # 'dayTemperatureLow': 31.44,
-              # 'dayToundedTemperatureLow': 31,
-              'formattedDayTemperatureLow': '31°',
-              'summary': 'Overcast',
-              'summaryIcon': 'cloudy',
-              # 'humidityFloat': 0.16,
-              # 'humidityPercent': 16,
-              'formattedHumidity': '16%',
-              'uvIndex': 2,
-              'uvExposureCategory': 'low',
-              # 'visibility': 10.00,
-              # 'visibilityUnits': 'miles'
-              'formattedVisibility': '10.00 miles',
-              'todaySummary': 'Overcast throughout the day.',
-              'next24HoursSummary': 'Possible light snow tomorrow evening.'
-            },
+            'time': '1583010500',
+            'timezoneOffset': -7,
+            'timezone': 'America/Denver',
+            'city': 'Denver',
+            'state': 'CO',
+            'country': 'United States',
+            'temperature': 54.3,
+            'feelsLike': 54.3,
+            'dayTemperatureHigh': 57.41,
+            'dayTemperatureLow': 31.44,
+            'summary': 'Overcast',
+            'icon': 'cloudy',
+            'humidity': 0.16,
+            'uvIndex': 3,
+            'uvExposureCategory': 'moderate',
+            'visibility': 10.00,
+            'todaySummary': 'Overcast throughout the day.',
+            'tonightSummary': '',
             'hourlyForecasts': [
               {
-                # 'hour': 3,
-                # 'period': 'PM',
-                'formattedHour': '3 PM',
-                # 'unixTimestamp': '1583013600',
-                # 'temperature': 55.19,
-                # 'roundedTemperature': 55,
-                'formattedTemperature': '55°',
-                'summaryIcon': 'cloudy'
+                'time': '1583013600',
+                'temperature': 55.19,
+                'icon': 'cloudy'
               },
               {
-                # 'hour': 4,
-                # 'period': 'PM',
-                'formattedHour': '4 PM',
-                # 'unixTimestamp': '1583017200',
-                # 'temperature': 56.88,
-                # 'roundedTemperature': 56,
-                'formattedTemperature': '56°',
-                'summaryIcon': 'cloudy'
+                'time': '1583017200',
+                'temperature': 56.88,
+                'icon': 'cloudy'
               },
               {
-                # 'hour': 5,
-                # 'period': 'PM',
-                'formattedHour': '5 PM',
-                # 'unixTimestamp': '1583020800',
-                # 'temperature': 55.93,
-                # 'roundedTemperature': 56,
-                'formattedTemperature': '56°',
-                'summaryIcon': 'cloudy'
+                'time': '1583020800',
+                'temperature': 55.93,
+                'icon': 'cloudy'
               },
               {
-                # 'hour': 6,
-                # 'period': 'PM',
-                'formattedHour': '6 PM',
-                # 'unixTimestamp': '1583024400',
-                # 'temperature': 51.8,
-                # 'roundedTemperature': 52,
-                'formattedTemperature': '52°',
-                'summaryIcon': 'cloudy'
+                'time': '1583024400',
+                'temperature': 51.8,
+                'icon': 'cloudy'
               },
               {
-                # 'hour': 7,
-                # 'period': 'PM',
-                'formattedHour': '7 PM',
-                # 'unixTimestamp': '1583028000',
-                # 'temperature': 47.56,
-                # 'roundedTemperature': 48,
-                'formattedTemperature': '48°',
-                'summaryIcon': 'cloudy'
+                'time': '1583028000',
+                'temperature': 47.56,
+                'icon': 'cloudy'
               },
               {
-                # 'hour': 8,
-                # 'period': 'PM',
-                'formattedHour': '8 PM',
-                # 'unixTimestamp': '1583031600',
-                # 'temperature': 44.58,
-                # 'roundedTemperature': 45,
-                'formattedTemperature': '45°',
-                'summaryIcon': 'cloudy'
+                'time': '1583031600',
+                'temperature': 44.58,
+                'icon': 'cloudy'
               },
               {
-                # 'hour': 9,
-                # 'period': 'PM',
-                'formattedHour': '9 PM',
-                # 'unixTimestamp': '1583035200',
-                # 'temperature': 42.47,
-                # 'roundedTemperature': 42,
-                'formattedTemperature': '42°',
-                'summaryIcon': 'cloudy'
+                'time': '1583035200',
+                'temperature': 42.47,
+                'icon': 'cloudy'
               },
               {
-                # 'hour': 10,
-                # 'period': 'PM',
-                'formattedHour': '10 PM',
-                # 'unixTimestamp': '1583035200',
-                # 'temperature': 41.17,
-                # 'roundedTemperature': 41,
-                'formattedTemperature': '41°',
-                'summaryIcon': 'cloudy'
+                'time': '1583035200',
+                'temperature': 41.17,
+                'icon': 'cloudy'
               }
-            ]
-            },
+            ],
             'dailyForecasts': [
               {
                 'weekday': 'Sunday',
-                # 'unixTimestamp': 1583046000,
-                'summaryIcon': 'snow',
-                # 'temperatureHigh': 51.99,
-                # 'roundedTemperatureHigh': 52,
-                'formattedTemperatureHigh': '52°',
-                # 'temperatureLow': 23.74,
-                # 'roundedTemperatureLow': 24,
-                'formattedTemperatureLow': '24°',
-                # 'humidityFloat': 0.63,
-                # 'humidityPercent': 63,
-                'formattedHumidity': '63%'
+                'time': 1583046000,
+                'icon': 'snow',
+                'temperatureHigh': 51.99,
+                'temperatureLow': 23.74,
+                'humidity': 0.63
               },
               {
                 'weekday': 'Monday',
-                # 'unixTimestamp': 1583132400,
-                'summaryIcon': 'snow',
-                # 'temperatureHigh': 44.69,
-                # 'roundedTemperatureHigh': 45,
-                'formattedTemperatureHigh': '45°',
-                # 'temperatureLow': 24.83,
-                # 'roundedTemperatureLow': 25,
-                'formattedTemperatureLow': '25°',
-                # 'humidityFloat': 0.75,
-                # 'humidityPercent': 75,
-                'formattedHumidity': '75%'
+                'time': 1583132400,
+                'icon': 'snow',
+                'temperatureHigh': 44.69,
+                'temperatureLow': 24.83,
+                'humidity': 0.75
               },
               {
                 'weekday': 'Tuesday',
-                # 'unixTimestamp': 1583218800,
-                'summaryIcon': 'clear-day',
-                # 'temperatureHigh': 56.47,
-                # 'roundedTemperatureHigh': 56,
-                'formattedTemperatureHigh': '56°',
-                # 'temperatureLow': 28.74,
-                # 'roundedTemperatureLow': 29,
-                'formattedTemperatureLow': '29°',
-                # 'humidityFloat': 0.51,
-                # 'humidityPercent': 51,
-                'formattedHumidity': '51%'
+                'time': 1583218800,
+                'icon': 'clear-day',
+                'temperatureHigh': 56.47,
+                'temperatureLow': 28.74,
+                'humidity': 0.51
               },
               {
                 'weekday': 'Wednesday',
-                # 'unixTimestamp': 1583305200,
-                'summaryIcon': 'clear-day',
-                # 'temperatureHigh': 65.56,
-                # 'roundedTemperatureHigh': 66,
-                'formattedTemperatureHigh': '66°',
-                # 'temperatureLow': 34.44,
-                # 'roundedTemperatureLow': 34,
-                'formattedTemperatureLow': '34°',
-                # 'humidityFloat': 0.33,
-                # 'humidityPercent': 33,
-                'formattedHumidity': '33%'
+                'time': 1583305200,
+                'icon': 'clear-day',
+                'temperatureHigh': 65.56,
+                'temperatureLow': 34.44,
+                'humidity': 0.33
               },
               {
                 'weekday': 'Thursday',
-                # 'unixTimestamp': 1583391600,
-                'summaryIcon': 'cloudy',
-                # 'temperatureHigh': 65.69,
-                # 'roundedTemperatureHigh': 66,
-                'formattedTemperatureHigh': '66°',
-                # 'temperatureLow': 32.56,
-                # 'roundedTemperatureLow': 33,
-                'formattedTemperatureLow': '33°',
-                # 'humidityFloat': 0.25,
-                # 'humidityPercent': 25,
-                'formattedHumidity': '25%'
+                'time': 1583391600,
+                'icon': 'cloudy',
+                'temperatureHigh': 65.69,
+                'temperatureLow': 32.56,
+                'humidity': 0.25
               }
             ]
           }
+        }
         })
       @expected = JSON.parse(expected_json)
     end
@@ -220,47 +127,46 @@ describe 'As a developer of the front-end' do
     describe 'I receive a JSON:API response' do
       describe 'with weather data about current conditions, including' do
         it 'current weather condition, matching icon, and current temperature' do
-          expect(@data['data']['attributes']['currently']['summary']). to eq(@expected['data']['attributes']['currently']['summary'])
-          expect(@data['data']['attributes']['currently']['summaryIcon']). to eq(@expected['data']['attributes']['currently']['summaryIcon'])
-          expect(@data['data']['attributes']['currently']['formattedTemperature']). to eq(@expected['data']['attributes']['currently']['formattedTemperature'])
+          expect(@data['data']['attributes']['summary']). to eq(@expected['data']['attributes']['summary'])
+          expect(@data['data']['attributes']['icon']). to eq(@expected['data']['attributes']['icon'])
+          expect(@data['data']['attributes']['formattedTemperature']). to eq(@expected['data']['attributes']['formattedTemperature'])
         end
 
         it 'feels like, humidity, visability, uv index and uv exposure category' do
-          expect(@data['data']['attributes']['currently']['formattedFeelsLike']). to eq(@expected['data']['attributes']['currently']['formattedFeelsLike'])
-          expect(@data['data']['attributes']['currently']['formattedHumidity']). to eq(@expected['data']['attributes']['currently']['formattedHumidity'])
-          expect(@data['data']['attributes']['currently']['formattedVisibility']). to eq(@expected['data']['attributes']['currently']['formattedVisibility'])
-          expect(@data['data']['attributes']['currently']['uvIndex']). to eq(@expected['data']['attributes']['currently']['uvIndex'])
-          expect(@data['data']['attributes']['currently']['uvExposureCategory']). to eq(@expected['data']['attributes']['currently']['uvExposureCategory'])
+          expect(@data['data']['attributes']['formattedFeelsLike']). to eq(@expected['data']['attributes']['formattedFeelsLike'])
+          expect(@data['data']['attributes']['formattedHumidity']). to eq(@expected['data']['attributes']['formattedHumidity'])
+          expect(@data['data']['attributes']['formattedVisibility']). to eq(@expected['data']['attributes']['formattedVisibility'])
+          expect(@data['data']['attributes']['uvIndex']). to eq(@expected['data']['attributes']['uvIndex'])
+          expect(@data['data']['attributes']['uvExposureCategory']). to eq(@expected['data']['attributes']['uvExposureCategory'])
         end
 
         it 'daily high and low, daily summary, and overnight summary' do
-          expect(@data['data']['attributes']['currently']['dayTemperatureLow']). to eq(@expected['data']['attributes']['currently']['dayTemperatureLow'])
-          expect(@data['data']['attributes']['currently']['dayTemperatureHigh']). to eq(@expected['data']['attributes']['currently']['dayTemperatureHigh'])
-          expect(@data['data']['attributes']['currently']['dayTemperatureHigh']). to eq(@expected['data']['attributes']['currently']['dayTemperatureHigh'])
-          expect(@data['data']['attributes']['currently']['todaySummary']). to eq(@expected['data']['attributes']['currently']['todaySummary'])
-          expect(@data['data']['attributes']['currently']['next24HoursSummary']). to eq(@expected['data']['attributes']['currently']['next24HoursSummary'])
+          expect(@data['data']['attributes']['dayTemperatureLow']). to eq(@expected['data']['attributes']['dayTemperatureLow'])
+          expect(@data['data']['attributes']['dayTemperatureHigh']). to eq(@expected['data']['attributes']['dayTemperatureHigh'])
+          expect(@data['data']['attributes']['dayTemperatureHigh']). to eq(@expected['data']['attributes']['dayTemperatureHigh'])
+          expect(@data['data']['attributes']['todaySummary']). to eq(@expected['data']['attributes']['todaySummary'])
+          expect(@data['data']['attributes']['next24HoursSummary']). to eq(@expected['data']['attributes']['next24HoursSummary'])
         end
       end
 
       it 'with data about city, state, country, time, and date' do
-        require "pry"; binding.pry
-        expect(@data['data']['attributes']['location']['formattedCityState']).to eq(@expected['data']['attributes']['location']['formattedCityState'])
-        expect(@data['data']['attributes']['location']['country']).to eq(@expected['data']['attributes']['location']['country'])
-        expect(@data['data']['attributes']['dateTime']['formattedDateTime']).to eq(@expected['data']['attributes']['dateTime']['formattedDateTime'])
+        expect(@data['data']['attributes']['formattedCityState']).to eq(@expected['data']['attributes']['formattedCityState'])
+        expect(@data['data']['attributes']['country']).to eq(@expected['data']['attributes']['country'])
+        expect(@data['data']['attributes']['formattedDateTime']).to eq(@expected['data']['attributes']['formattedDateTime'])
       end
 
       it 'with hourly temperature forecast for next 8 hours' do
         @data['data']['attributes']['hourlyForecasts'].each_with_index do |hourly, index|
           expect(hourly['formattedHour']).to eq(@expected['data']['attributes']['hourlyForecasts'][index]['formattedHour'])
           expect(hourly['formattedTemperature']).to eq(@expected['data']['attributes']['hourlyForecasts'][index]['formattedTemperature'])
-          expect(hourly['summaryIcon']).to eq(@expected['data']['attributes']['hourlyForecasts'][index]['summaryIcon'])
+          expect(hourly['icon']).to eq(@expected['data']['attributes']['hourlyForecasts'][index]['icon'])
         end
       end
 
       it 'with weather summary, icon, high and low temps data for next 5 days' do
         @data['data']['attributes']['dailyForecasts'].each_with_index do |daily, index|
           expect(daily['weekday']).to eq(@expected['data']['attributes']['dailyForecasts'][index]['weekday'])
-          expect(daily['summaryIcon']).to eq(@expected['data']['attributes']['dailyForecasts'][index]['summaryIcon'])
+          expect(daily['icon']).to eq(@expected['data']['attributes']['dailyForecasts'][index]['icon'])
           expect(daily['formattedTemperatureHigh']).to eq(@expected['data']['attributes']['dailyForecasts'][index]['formattedTemperatureHigh'])
           expect(daily['formattedTemperatureLow']).to eq(@expected['data']['attributes']['dailyForecasts'][index]['formattedTemperatureLow'])
           expect(daily['formattedHumidity']).to eq(@expected['data']['attributes']['dailyForecasts'][index]['formattedHumidity'])
