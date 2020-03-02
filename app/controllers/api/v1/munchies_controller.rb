06369ee7bdef95044880f1a3950ba268cbfe7005
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-02 10:03:21 -0700

    Update munchies controller with API calls thus far

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,9 +1,24 @@
 class Api::V1::MunchiesController < ApplicationController
   def show
-    # start_location_info = GoogleApiService.new.geocode(params[:start])
+    # Get necessary Google API info
     end_location_info = GoogleApiService.new.geocode(params[:end])
     distance_info = GoogleApiService.new.distance(params[:start], params[:end])
-    require "pry"; binding.pry
+
+    # Calculate Unix timestamp of arrival time
+    travel_seconds = distance_info['rows'][0]['elements'][0]['duration']['value']
+    arrival_timestamp = Time.now.to_i + travel_seconds
+
+    # Pull latitude and longitude from geocode info
+    lat = end_location_info['results'][0]['geometry']['location']['lat']
+    long = end_location_info['results'][0]['geometry']['location']['lng']
+
+    # Get DarkSky forecast at given arrival time
+    forecast_info = DarkSkyService.new.forecast(lat, long, arrival_timestamp)
+    forecast = forecast_info['currently']['summary']
+
+    # Get Yelp info on restaurant at end location open at timestamp
+    restaurant_info = YelpService.new.restaurant_open_at(params[:food], arrival_timestamp)
+
     munchies = Munchies.new(params[:start], params[:end], params[:food])
     render json: MunchiesSerializer.new(munchies)
   end
