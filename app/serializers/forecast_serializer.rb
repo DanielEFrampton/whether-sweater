b7662ed8fcb959c872e1fdaee8771074bc96df0f
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-01 18:29:58 -0700

    Add attributes to Forecast serializer

--- app/serializers/forecast_serializer.rb
+++ app/serializers/forecast_serializer.rb
@@ -1,4 +1,27 @@
 class ForecastSerializer
   include FastJsonapi::ObjectSerializer
   set_key_transform :camel_lower
+  attributes :city,
+             :state,
+             :country,
+             :time,
+             :timezone_offset,
+             :timezone,
+             :city,
+             :state,
+             :country,
+             :temperature,
+             :feelsLike,
+             :day_temperature_high,
+             :day_temperature_low,
+             :summary,
+             :icon,
+             :humidity,
+             :uv_index,
+             :uv_exposure_category,
+             :visibility,
+             :today_summary,
+             :tonight_summary,
+             :hourly_forecasts,
+             :daily_forecasts
 end
