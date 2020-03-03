02f2bbc655feac68e969fb44c5a0b19b60c940cf
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-03 10:43:22 -0700

    Create error serializer

---
+++ app/serializers/error_serializer.rb
@@ -0,0 +1,18 @@
+class ErrorSerializer
+  def initialize(error, status, param)
+    @error = error
+    @status = status
+    @parameter = param
+  end
+
+  def errors
+    {
+      errors: [
+        status: @status.to_s,
+        source: { pointer: '/api/v1/sessions', parameter: @parameter },
+        title: 'Invalid Authentication',
+        detail: @error
+      ]
+    }
+  end
+end
