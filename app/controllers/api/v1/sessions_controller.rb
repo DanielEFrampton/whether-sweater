c2a1e44079597c51717d5de238c5c2d7358e19c8
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-03 15:10:30 -0700

    Correct logic error in sessions controller caused by refactor

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::SessionsController < ApplicationController
   def create
-    if User.find_by(email: params[:email]).none?
+    if !user = User.find_by(email: params[:email])
       render json: ErrorSerializer.new(pointer: request.env['PATH_INFO'], title: 'Invalid Credentials', detail: 'Invalid email.', status: credential_failure_status, parameter: 'email').errors, status: credential_failure_status
     elsif user.authenticate(params[:password])
       render json: UserSerializer.new(user)
