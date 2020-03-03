30503b09d18ed2ca3f87c4baa90feb13566ae92b
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-03 14:53:54 -0700

    Refactor road trip controller to check for valid API key

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,22 +1,30 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
-    end_location = GoogleApiService.new.geocode(params[:destination])
-    distance_info = GoogleApiService.new.distance(params[:origin], params[:destination])
-    forecast_info = DarkSkyService.new.simple_future_forecast(end_location[:lat], end_location[:long], distance_info[:arrival_timestamp])
-    summary = forecast_info['currently']['summary']
-    temp = forecast_info['currently']['temperature']
-    forecast = FutureForecast.new(summary, temp)
-    road_trip = RoadTrip.new(forecast: forecast,
-                             travel_time: distance_info[:travel_time],
-                             origin: format_location(params[:origin]),
-                             destination: format_location(params[:destination]))
-    render json: RoadTripSerializer.new(road_trip), status: 201
+    if !params[:api_key]
+      status = 401
+      render json: ErrorSerializer.new(title: 'Unauthorized Request', detail: 'No API key was provided.', status: status, parameter: 'api_key', pointer: request.env['PATH_INFO']).errors, status: status
+    elsif User.authenticate_token(params[:api_key])
+      end_location = GoogleApiService.new.geocode(params[:destination])
+      distance_info = GoogleApiService.new.distance(params[:origin], params[:destination])
+      forecast_info = DarkSkyService.new.simple_future_forecast(end_location[:lat], end_location[:long], distance_info[:arrival_timestamp])
+      summary = forecast_info['currently']['summary']
+      temp = forecast_info['currently']['temperature']
+      forecast = FutureForecast.new(summary, temp)
+      road_trip = RoadTrip.new(forecast: forecast,
+                               travel_time: distance_info[:travel_time],
+                               origin: format_location(params[:origin]),
+                               destination: format_location(params[:destination]))
+      render json: RoadTripSerializer.new(road_trip), status: 201
+    else
+      status = 401
+      render json: ErrorSerializer.new(title: 'Unauthorized Request', detail: 'The API key provided was invalid.', status: status, parameter: 'api_key', pointer: request.env['PATH_INFO']).errors, status: status
+    end
   end
 
   private
 
   def strong_params
-    params.permit(:origin, :destination)
+    params.permit(:origin, :destination, :api_key)
   end
 
   def format_location(string)
