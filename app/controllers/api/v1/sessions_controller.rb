2c0dede9df71e8e48c999eae9587a01cb4d6bf16
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-03 16:38:53 -0700

    Rearrange sessions controller to limit line length

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,11 +1,23 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     if !user = User.find_by(email: params[:email])
-      render json: ErrorSerializer.new(pointer: request.env['PATH_INFO'], title: 'Invalid Credentials', detail: 'Invalid email.', status: credential_failure_status, parameter: 'email').errors, status: credential_failure_status
+      render json: ErrorSerializer.new(pointer: request.env['PATH_INFO'],
+                                       title: 'Invalid Credentials',
+                                       detail: 'Invalid email.',
+                                       status: credential_failure_status,
+                                       parameter: 'email')
+                                       .errors,
+                                       status: credential_failure_status
     elsif user.authenticate(params[:password])
       render json: UserSerializer.new(user)
     else
-      render json: ErrorSerializer.new(pointer: request.env['PATH_INFO'], title: 'Invalid Credentials', detail: 'Invalid password.', status: credential_failure_status, parameter: 'password').errors, status: credential_failure_status
+      render json: ErrorSerializer.new(pointer: request.env['PATH_INFO'],
+                                       title: 'Invalid Credentials',
+                                       detail: 'Invalid password.',
+                                       status: credential_failure_status,
+                                       parameter: 'password')
+                                       .errors,
+                                       status: credential_failure_status
     end
   end
 
