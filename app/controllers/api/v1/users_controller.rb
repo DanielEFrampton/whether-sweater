158315752b0b0c974e8969318716b2493e9bdabe
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-02 22:54:14 -0700

    Specify status code 201 on successful user creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     user = User.new(strong_params)
     if user.save
-      render json: UserSerializer.new(user)
+      render json: UserSerializer.new(user), status: 201
     else
       render status: 400
     end
