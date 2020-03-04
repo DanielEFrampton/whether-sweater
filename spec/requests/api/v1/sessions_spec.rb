a0f49d4e142745521f0521d85552c0737862e332
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-03 19:25:49 -0700

    Add test for scenario where email is wrong on login

--- spec/requests/api/v1/sessions_spec.rb
+++ spec/requests/api/v1/sessions_spec.rb
@@ -30,16 +30,15 @@ describe 'as a front-end developer' do
     end
 
     describe 'with an invalid username and/or password' do
-      before(:each) do
-        @user = create(:user)
-        post '/api/v1/sessions', params: {"email": @user.email,
-                                          "password": "wrong_password"}
-        @status_code = response.status
-        @response = JSON.parse(response.body)
-      end
+      it 'I receive a JSON:API response with a description of why it failed and 400 status code' do
+        user_1 = create(:user)
+        post '/api/v1/sessions', params: {"email": user_1.email,
+          "password": "wrong_password"}
+        status_code_1 = response.status
+        response_1 = JSON.parse(response.body)
 
-      it 'I receive a JSON:API response with a description of why it failed' do
-        expect(@response).to include(
+        expect(status_code_1).to eq(400)
+        expect(response_1).to include(
           'errors' => [
             "status" => "400",
             "source" => { "pointer" => "/api/v1/sessions", "parameter" => "password" },
@@ -47,10 +46,22 @@ describe 'as a front-end developer' do
             "detail" => "Invalid password."
           ]
         )
-      end
 
-      it "I receive a 400-level status code" do
-        expect(@status_code).to eq(400)
+        user_2 = create(:user, email: 'the@right.email')
+        post '/api/v1/sessions', params: {"email": 'not@theright.email',
+          "password": "password"}
+        status_code_2 = response.status
+        response_2 = JSON.parse(response.body)
+
+        expect(status_code_2).to eq(400)
+        expect(response_2).to include(
+          'errors' => [
+            "status" => "400",
+            "source" => { "pointer" => "/api/v1/sessions", "parameter" => "email" },
+            "title" =>  "Invalid Credentials",
+            "detail" => "Invalid email."
+          ]
+        )
       end
     end
   end
