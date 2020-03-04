21c069df9c236f72e1e8ecf04ae974935b4a0dca
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-03 18:06:39 -0700

    Refactor sessions controller to encapsulate error generation

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,20 +1,12 @@
 class Api::V1::SessionsController < ApplicationController
   def create
     if !user = User.find_by(email: params[:email])
-      render json: Error.new(pointer: request.env['PATH_INFO'],
-                             title: 'Invalid Credentials',
-                             detail: 'Invalid email.',
-                             status: credential_failure_status,
-                             parameter: 'email').errors,
+      render json: generate_error('Invalid email.', 'email'),
              status: credential_failure_status
     elsif user.authenticate(params[:password])
       render json: UserSerializer.new(user)
     else
-      render json: Error.new(pointer: request.env['PATH_INFO'],
-                             title: 'Invalid Credentials',
-                             detail: 'Invalid password.',
-                             status: credential_failure_status,
-                             parameter: 'password').errors,
+      render json: generate_error('Invalid password.', 'password'),
              status: credential_failure_status
     end
   end
@@ -23,5 +15,13 @@ class Api::V1::SessionsController < ApplicationController
 
   def credential_failure_status
     400
+  end
+
+  def generate_error(detail, parameter)
+    Error.new(pointer: request.env['PATH_INFO'],
+              title: 'Invalid Credentials',
+              detail: detail,
+              status: credential_failure_status,
+              parameter: parameter).errors
   end
 end
