5e2c5f82b94c6dc87f9d0f228aa81d93c57eccec
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-03 18:21:49 -0700

    Refactor munchies controller to reduce controller action size

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -1,18 +1,12 @@
 class Api::V1::MunchiesController < ApplicationController
   def show
-    end_location = GoogleApiService.new.geocode(params[:end])
-    distance = GoogleApiService.new.distance(params[:start], params[:end])
-    forecast = DarkSkyService.new.future_forecast(end_location[:lat],
-                                                  end_location[:long],
-                                                  distance[:arrival_time])
-    restaurant = YelpService.new.restaurant_open_at(end_location[:lat],
-                                                    end_location[:long],
-                                                    params[:food],
-                                                    distance[:arrival_time])
-    munchies = Munchies.new(forecast.summary,
-                            restaurant,
-                            distance[:travel_time],
-                            end_location[:city_state])
+    munchies = Munchies.new(strong_params)
     render json: MunchiesSerializer.new(munchies)
+  end
+
+  private
+
+  def strong_params
+    params.permit(:start, :end, :food)
   end
 end
