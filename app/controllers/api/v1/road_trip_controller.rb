b7ad090d61a419e5f5b38d054cd8145ec9f3e9ba
Daniel Frampton <daniel.e.frampton@gmail.com>
2020-03-03 18:32:51 -0700

    Encapsulate logic from road_trip controller in road_trip poro

--- app/controllers/api/v1/road_trip_controller.rb
+++ app/controllers/api/v1/road_trip_controller.rb
@@ -1,39 +1,32 @@
 class Api::V1::RoadTripController < ApplicationController
   def create
     if !params[:api_key]
-      status = 401
-      render json: Error.new(title: 'Unauthorized Request',
-                             detail: 'No API key was provided.',
-                             status: status,
-                             parameter: 'api_key',
-                             pointer: request.env['PATH_INFO']).errors,
-             status: status
+      render json: generate_error('No API key was provided.'),
+             status: request_failure_status
     elsif User.authenticate_token(params[:api_key])
-      end_location = GoogleApiService.new.geocode(params[:destination])
-      distance = GoogleApiService.new.distance(params[:origin],
-                                               params[:destination])
-      forecast = DarkSkyService.new.future_forecast(end_location[:lat],
-                                                    end_location[:long],
-                                                    distance[:arrival_time])
-      road_trip = RoadTrip.new(forecast: forecast,
-                               travel_time: distance[:travel_time],
-                               origin: city_state(params[:origin]),
-                               destination: city_state(params[:destination]))
+      road_trip = RoadTrip.new(strong_params)
       render json: RoadTripSerializer.new(road_trip), status: 201
     else
-      status = 401
-      render json: Error.new(title: 'Unauthorized Request',
-                             detail: 'The API key provided was invalid.',
-                             status: status,
-                             parameter: 'api_key',
-                             pointer: request.env['PATH_INFO']).errors,
-             status: status
+      render json: generate_error('The API key provided was invalid.'),
+             status: request_failure_status
     end
   end
 
   private
 
-  def city_state(string)
-    string.gsub(',', ', ')
+  def strong_params
+    params.permit(:origin, :destination)
+  end
+
+  def request_failure_status
+    401
+  end
+
+  def generate_error(detail)
+    Error.new(title: 'Unauthorized Request',
+              detail: detail,
+              status: request_failure_status,
+              parameter: 'api_key',
+              pointer: request.env['PATH_INFO']).errors
   end
 end
--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -1,11 +1,36 @@
 class RoadTrip
   attr_reader :id, :forecast, :travel_time, :origin, :destination
 
-  def initialize(forecast:, travel_time:, origin:, destination:)
+  # def initialize(forecast:, travel_time:, origin:, destination:)
+  def initialize(params)
     @id = nil
-    @forecast = forecast
-    @travel_time = travel_time
-    @origin = origin
-    @destination = destination
+
+    @start = params[:origin]
+    @end = params[:destination]
+
+    @forecast = generate_forecast
+    @travel_time = distance_info[:travel_time]
+    @origin = city_state(@start)
+    @destination = city_state(@end)
+  end
+
+  private
+
+  def city_state(string)
+    string.gsub(',', ', ')
+  end
+
+  def end_location_info
+    @end_location_info ||= GoogleApiService.new.geocode(@end)
+  end
+
+  def distance_info
+    @distance_info ||= GoogleApiService.new.distance(@start, @end)
+  end
+
+  def generate_forecast
+    DarkSkyService.new.future_forecast(end_location_info[:lat],
+                                       end_location_info[:long],
+                                       distance_info[:arrival_time])
   end
 end
